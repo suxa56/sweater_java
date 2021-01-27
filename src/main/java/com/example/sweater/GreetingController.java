8c960c3329b7792fd9fa8c070f2f2fc3a0816820
suxa56 <suxa5637@mail.ru>
2021-01-27 12:42:12 +0500

    filter

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -4,7 +4,6 @@ import com.example.sweater.domain.Message;
 import com.example.sweater.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,7 @@ public class GreetingController {
     private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
@@ -33,12 +32,30 @@ public class GreetingController {
 
     @PostMapping
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+//        Создает объект класса Message и сохраняет его через метод save
         Message message = new Message(text, tag);
         messageRepo.save(message);
 
+//        И сразу, без перезагрузок выводит сообщения
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
+        return "main";
+    }
+
+//    @PostMapping("filter") связан с тегом form и атрибутом action
+//    Метод ищет сообщения по String filter, который передается в тэге input, атрибуте name
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+//        Если фильтр не пустой, то возвращается результат
+//        Если фильтр пустой, то он возвращает все сообщения
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 
--- src/main/java/com/example/sweater/repo/MessageRepo.java
+++ src/main/java/com/example/sweater/repo/MessageRepo.java
@@ -3,5 +3,9 @@ package com.example.sweater.repo;
 import com.example.sweater.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends JpaRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
