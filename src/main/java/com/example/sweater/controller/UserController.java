e2a4aa21f0c3bd5e5d85a3e351f59bc13596e972
suxa56 <suxa5637@mail.ru>
2021-02-03 14:12:36 +0500

    migrations and profile updates

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -2,67 +2,76 @@ package com.example.sweater.controller;
 
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
-import com.example.sweater.repo.UserRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-//Есть доступ только для пользователей с ролью ADMIN
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
+
 //      Современный вариант без @Autowired
-//    private final UserRepo userRepo;
+//    private final UserService userService;
 //
-//    public UserService(UserRepo userRepo) {
-//        this.userRepo = userRepo;
+//    public UserService(UserService userService) {
+//        this.userService = userService;
 //    }
 
+//    Есть доступ только для пользователей с ролью ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+//    Есть доступ только для пользователей с ролью ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
-
+//    Есть доступ только для пользователей с ролью ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
 //    Редактирование пользователя, его имя и роль
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user
-            ) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+    ) {
+        userService.saveUser(user, username, form);
         return "redirect:/user";
+    }
+
+//    Мэппинг для страницы профиля
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+         model.addAttribute("username", user.getUsername());
+         model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+//    Мэппинг для обновления профиля
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ) {
+        userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -50,21 +50,12 @@ public class UserService implements UserDetailsService {
 //        При совпадении почта будет подтверждена
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
+//        Отправляет письмо для подтверждения
+        sendMessage(user);
 
-//        Если поле почты не пустой, то отправляет пользователю почту
-        if (!StringUtils.isEmpty(user.getEmail())) {
-            String message = String.format(
-                    "Hello, %s! \n" +
-                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
-                    user.getUsername(),
-                    user.getActivationCode()
-            );
-
-//            отправляет письмо на почту(user,getEmail()), с темой "Activation", и текстом message
-            mailSender.send(user.getEmail(), "Activation", message);
-        }
         return true;
     }
+
 
     public boolean activateUser(String code) {
 //        Ищет пользователя по коду активации
@@ -82,4 +73,80 @@ public class UserService implements UserDetailsService {
 
         return true;
     }
+
+//    Поиск всех пользователей
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+//        Получаем список ролей
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+//        Убираем роли у пользователя, для добавления по новой
+        user.getRoles().clear();
+
+//        Добавление ролей
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+    }
+
+//    Обновление профиля
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+//        Проверка и изменение почты
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+//        Если почта поменялась, то обновляем у профился
+//        Если пользователь не удалил почту, то задаем код активации
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)) {
+               user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+//        Если пароль не пуст, то обновляем его
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+//        Сохраняем пользователя
+        userRepo.save(user);
+
+//        Если почта изменена, то высылаем код активации для подтверждения
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
+    }
+
+
+    private void sendMessage(User user) {
+        //        Если поле почты не пустой, то отправляет пользователю почту
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+//            отправляет письмо на почту(user,getEmail()), с темой "Activation", и текстом message
+            mailSender.send(user.getEmail(), "Activation", message);
+        }
+    }
 }
+
+
