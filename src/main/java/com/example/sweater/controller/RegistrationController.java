481c03e6c43b596a356e16154ec34b0255cc3f00
suxa56 <suxa5637@mail.ru>
2021-02-08 21:10:01 +0500

    reCaptcha

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.domain.User;
+import com.example.sweater.domain.dto.CaptchaResponseDto;
 import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,15 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     @Autowired
     private UserService userService;
-
 //      Современный вариант без @Autowired
 //    private final UserService userService;
 //
@@ -27,17 +33,37 @@ public class RegistrationController {
 //        this.userService = userService;
 //    }
 
+    @Autowired
+    private RestTemplate restTemplate;
+//      Современный вариант без @Autowired
+//    private final RestTemplate restTemplate;
+//
+//    public RegistrationController(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
-//    Регистрация нового пользователся
+    //    Регистрация нового пользователся
 //    BindingResult bindingResult всегда ставится спереди модели (Model model)
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponce,
             @Valid User user, BindingResult bindingResult, Model model) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponce);
+        CaptchaResponseDto responce = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!responce.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+
         boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
 
         if (isConfirmEmpty) {
@@ -48,7 +74,7 @@ public class RegistrationController {
             model.addAttribute("passwordError", "Passwords are different!");
         }
 
-        if (isConfirmEmpty || bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors() || !responce.isSuccess()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
